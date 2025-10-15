fe39ec7959d411b08342e86c9bf58ead42a02d92
Andrei Sevaka <severiniulia2@gmail.com>
2025-10-15 22:32:21 +0200

    v2 con comentarios-JQuery

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,31 +1,43 @@
 $(document).ready(function () {
 
-  // Al cargar, solo mostramos la sección de inicio
+  // Al cargar, mostrar solo la sección de inicio
   $("#inicio").show();
   $("#about, #resources, #contact").hide();
 
-  // Botón INICIO
+  // Botón Inicio
   $("#btnInicio").click(function () {
     $("#inicio").show();
     $("#about, #resources, #contact").hide();
   });
 
-  // Botón ABOUT
+  // Botón Sobre la ludopatía
   $("#btnAbout").click(function () {
     $("#about").show();
     $("#inicio, #resources, #contact").hide();
   });
 
-  // Botón RESOURCES
+  // Botón Recursos
   $("#btnResources").click(function () {
     $("#resources").show();
     $("#inicio, #about, #contact").hide();
   });
 
-  // Botón CONTACT
+  // Botón Contacto
   $("#btnContact").click(function () {
     $("#contact").show();
     $("#inicio, #about, #resources").hide();
   });
 
+  // Validar formulario y mostrar modal
+  $("#formContacto").submit(function (e) {
+    e.preventDefault();
+    const correo = $("#correo").val();
+
+    if (/\S+@\S+\.\S+/.test(correo)) {
+      $("#modalConfirm").modal("show");
+      this.reset();
+    } else {
+      alert("Por favor, introduce un correo válido.");
+    }
+  });
 });
